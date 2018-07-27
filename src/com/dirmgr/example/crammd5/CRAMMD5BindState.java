dc256ca9b7a0b26b24526834b47334c0d7ad8fd1
Neil Wilson <nawilson@naw-serval>
2018-07-26 23:31:51 -0500

    Update licenses
    Updated the set of licenses that cover the code in this repository.
    It was previously listed as being covered only by GPLv2.  It is now
    also covered by LGPLv2.1 and Apache License version 2.0.  The source
    headers now reflect this.

--- src/com/dirmgr/example/crammd5/CRAMMD5BindState.java
+++ src/com/dirmgr/example/crammd5/CRAMMD5BindState.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2018 Ping Identity Corporation
+ * Copyright 2018 Neil A. Wilson
  * All Rights Reserved.
  */
 /*
- * Copyright (C) 2018 Ping Identity Corporation
+ * Copyright (C) 2018 Neil A. Wilson
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License (GPLv2 only)
@@ -17,6 +17,21 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+/*
+ * Copyright 2018 Neil A. Wilson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.dirmgr.example.crammd5;
 
